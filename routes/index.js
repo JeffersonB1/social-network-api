99a152b478ecd1591dfe60f8faca9d394d942126
shiba inu <jefferson121996@gmail.com>
2022-07-13 15:42:33 -0400

    user and thought  routes changed

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const apiRoutes = require('./api/index');
 
 router.use('/api', apiRoutes);
 
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+})
+
 module.exports = router;
