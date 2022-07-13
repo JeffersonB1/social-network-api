53131b4f708f88764b88315b2e17309947a77025
shiba inu <jefferson121996@gmail.com>
2022-07-13 15:07:18 -0400

    setting up the routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,48 @@
 const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionBody: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: () => Date.now()
+        },
+        updatedAt: {
+            type: Date,
+            default: () => Date.now()
+        }
+    }
+);
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+    reactions: [reactionSchema],
+});
+
+module.exports = Thought;
