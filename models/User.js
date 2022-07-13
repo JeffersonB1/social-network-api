3028c27c5740905e73d2131d65db3e70ef1c04a1
shiba inu <jefferson121996@gmail.com>
2022-07-13 14:46:44 -0400

    user model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,46 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose, SchemaTypes } = require('mongoose');
 
-const 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        validate: {
+            validator: function (v) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            },
+            message: (props) => `${props.value} invalid email address`,
+        },
+    },
+    thoughts: [
+        {
+            type: SchemaTypes.ObjectId,
+            ref: "Thought"
+        },
+    ],
+    friends: [
+        {
+            type: SchemaTypes.ObjectId,
+            ref: "User"
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+
+
+
+});
+
+userSchema.virtual("friendCount").get(function () {
+    return `${this.friends.length} friends`;
+  });
+
+  module.exports = User;
