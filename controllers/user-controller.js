39d89d2ac4b7a1cad3097cdb493b7913e79f3b61
shiba inu <jefferson121996@gmail.com>
2022-07-14 18:46:48 -0400

    getuserbyid and createuser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,5 +12,29 @@ const userController = {
         })
     },
 
-    
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate([
+            { path: 'thoughts', select: "-__v"},
+            { path: 'friends', select: "-__v" }
+        ])
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
 }
