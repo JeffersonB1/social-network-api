580d713e41ad43be273ba26c5b9f85442db78558
shiba inu <jefferson121996@gmail.com>
2022-07-14 20:15:30 -0400

    working on thoghtcontroller, getallthought and getthoughtbyid

---
+++ controllers/thought-controller.js
@@ -0,0 +1,34 @@
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+
+    getAllThoughts(res, res) {
+        Thought.find({})
+        .populate({ path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate({ path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    
+}
